201ffbf22e8d6f182929d527d43a0e1c947f0b9a
Kesler Mathieu <kmat5@icloud.com>
2020-08-07 03:57:14 -0400

    changing data type of keys

--- realtime_feed_urls.py
+++ realtime_feed_urls.py
@@ -1,17 +1,15 @@
 BASELINE_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs"
 
+
 FEED_URLS = {
 
-    "123456": BASELINE_URL,
-    "ACE": BASELINE_URL + "-ace",
-    "G": BASELINE_URL + "-g",
-    "NQRW": BASELINE_URL + "-nqrw",
-    "SIR": BASELINE_URL + "-si",
-    "BDFM": BASELINE_URL + "-bdfm",
-    "JZ": BASELINE_URL + "-jz",
-    "L": BASELINE_URL + "-l",
-    "7": BASELINE_URL + "-7"
+    ('1', '2', '3', '4', '5', '6'): BASELINE_URL,
+    ('A', 'C', 'E'): BASELINE_URL + "-ace",
+    ('G',): BASELINE_URL + "-g",
+    ('N', 'Q', 'R', 'W'): BASELINE_URL + "-nqrw",
+    ('B', 'D', 'F', 'M'): BASELINE_URL + "-bdfm",
+    ('J', 'Z'): BASELINE_URL + "-jz",
+    ('L',): BASELINE_URL + "-l",
+    ('7',): BASELINE_URL + "-7"
 
 }
-
-#TODO: write function that given char of train, returns the corresponding url
