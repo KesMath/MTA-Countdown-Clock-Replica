ea5f50b58e539255c77e1dd461ded2b95ee54767
Kesler Mathieu <kmat5@icloud.com>
2020-08-09 21:51:41 -0400

    adding new exception

--- util/custom_exceptions.py
+++ util/custom_exceptions.py
@@ -2,3 +2,8 @@
 class FileExtensionException(Exception):
     """Raised when a given file extension does not match expected"""
     pass
+
+
+class RouteIdException(Exception):
+    """Raised when a route id/train number does not exist or is not supported by app"""
+    pass
