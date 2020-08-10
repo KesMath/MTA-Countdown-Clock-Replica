2f81832c8860a9fa1305ba74321475b34f9de570
Kesler Mathieu <kmat5@icloud.com>
2020-08-09 21:20:31 -0400

    adding helper function to fetch URL given routeid

--- realtime_feed_urls.py
+++ realtime_feed_urls.py
@@ -13,3 +13,13 @@ FEED_URLS = {
     ('7',): BASELINE_URL + "-7"
 
 }
+
+def get_url(route_id):
+    for key, value in FEED_URLS.items():
+        for route in key:
+            if route_id is route:
+                return FEED_URLS.get(key)
+
+
+if __name__ == '__main__':
+    print(get_url('N'))
