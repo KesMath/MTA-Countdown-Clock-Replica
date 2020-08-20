176b97c01ad87fb14817f2cc3f7ece3f99bc67d6
Kesler Mathieu <kmat5@icloud.com>
2020-08-19 20:26:30 -0400

    generalizing file IO functions

--- util/os_func.py
+++ util/os_func.py
@@ -23,3 +23,43 @@ def parse_file_name(abs_file_path):
 
     list_split = abs_file_path.split(path_delim)
     return list_split[len(list_split)-1].split('.')[0]
+
+
+def read_file(absolute_path):
+    """
+    :param absolute_path: full path including file name
+    :return: entire file is returned as a list of strings
+    """
+    if os.path.isfile(absolute_path) is True:
+        try:
+            with open(absolute_path, "r") as f:
+                data = f.readlines()
+                return data
+
+        except OSError as e:
+            print(e)
+
+    else:
+        raise OSError(absolute_path + " is an invalid file!")
+
+
+def write_file(relative_path, name, mode, content):
+    """
+    :param relative_path:
+    :param name:
+    :param mode:
+    :param content:
+    :return:
+    """
+    #TODO: prevent special characters from being passed through name parameter
+    #TODO: need to limit characters being passed in to mode param
+
+    if os.path.isdir(relative_path) is True:
+        with open(os.path.join(relative_path + name), mode) as f:
+            try:
+                f.write(content)
+
+            except OSError as e:
+                print(e)
+    else:
+        raise OSError(relative_path + " is an invalid directory!")
