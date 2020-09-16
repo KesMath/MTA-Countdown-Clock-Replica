6fe8ce5061874d2e6562b2de45327a31ce4130ea
Kesler Mathieu <kmat5@icloud.com>
2020-09-16 17:29:03 -0400

    adding test code for timestamp verfication

--- util/timestamp_converter.py
+++ util/timestamp_converter.py
@@ -4,3 +4,21 @@ def convert_timestamp_to_datetime(timestamp):
     return datetime.datetime.fromtimestamp(
         int(timestamp)
     ).strftime('%m-%d-%Y %H:%M:%S')
+
+
+if __name__ == '__main__':
+    timestamps = [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+    ]
+    for time in timestamps: print(convert_timestamp_to_datetime(time))
+
+
