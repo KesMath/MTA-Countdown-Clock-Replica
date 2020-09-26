c67cc9765abdf59502874c857f10d80cc3aaa173
Kesler Mathieu <kmat5@icloud.com>
2020-09-26 17:10:15 -0400

    adding rounding logic for accurate minute tracking

--- util/timestamp_operators.py
+++ util/timestamp_operators.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 
 def convert_timestamp_to_datetime(timestamp):
     return datetime.datetime.fromtimestamp(
@@ -6,9 +7,18 @@ def convert_timestamp_to_datetime(timestamp):
     ).strftime('%m-%d-%Y %H:%M:%S')
 
 def compute_relative_time(timestamp):
-    """subtracts current timestamp from param timestamp and converts value into minutes"""
+    """subtracts current timestamp from param timestamp and converts value into minutes.
+    performs necessary minute rounding (i.e. rounds up if remainder >= 0.5 else rounds down)"""
 
-    #FIXME: casting is causing precision lost.. need to implement floor / ceil function for better time accuracy!
-    return int((timestamp - datetime.datetime.now().timestamp())/60) \
-                            if timestamp > datetime.datetime.now().timestamp() else -1
+    #NOTE: uncomment code to perform minute analysis to assure rounding accuracy
+    #if timestamp > datetime.datetime.now().timestamp():
+        #print((timestamp - datetime.datetime.now().timestamp())/60)
 
+    if timestamp > datetime.datetime.now().timestamp():
+        if ((timestamp - datetime.datetime.now().timestamp())/60) % \
+                int(((timestamp - datetime.datetime.now().timestamp())/60)) < 0.5:
+            return math.floor(((timestamp - datetime.datetime.now().timestamp())/60))
+        else:
+            return math.ceil(((timestamp - datetime.datetime.now().timestamp())/60))
+    else:
+        return -1
