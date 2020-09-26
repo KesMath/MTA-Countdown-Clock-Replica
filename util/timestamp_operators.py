ad821ec6ac79bbaebb6ea21320ccb3b2779332ce
Kesler Mathieu <kmat5@icloud.com>
2020-09-26 18:43:39 -0400

    adding note about rounding threshold

--- util/timestamp_operators.py
+++ util/timestamp_operators.py
@@ -1,6 +1,27 @@
 import datetime
 import math
 
+SEC_TO_MINUTE = 60
+
+'''NOTE: may need to adjust rounding threshold for departure/ arrival times!
+   'common rounding technique' may be too optimistic and it's best to err on side of pessimism!  
+   
+   EXAMPLE: If you are rushing to make the train and time is 2.5 til departure,
+   would you rather api round down to 2 minutes so you can rush and make it versus rounding up to 
+   3 minutes so you can think you have more time then you have in actuality?
+   In this case we have a bias for the lowest integer.
+   
+   EXAMPLE: If you are waiting for a train arrival and time is 2.5 til arrival,
+   would you rather the api round down to 2 minutes so you can be over-optimistic
+   and have a false sense of hope versus rounding up to 3 minutes to avoid disappointment and
+   provide more room for possible delays. In this case we have a bias for the highest integer.
+   
+   FYI: this is not critical in the grand scheme of timing as (+ or -) 1 minute is not a major difference.
+   The rounding threshold just adjusts INTEGER BIAS when rounding up or rounding down
+   
+'''
+ROUNDING_THRESHOLD = 0.5
+
 def convert_timestamp_to_datetime(timestamp):
     return datetime.datetime.fromtimestamp(
         int(timestamp)
@@ -12,15 +33,17 @@ def compute_relative_time(timestamp):
 
     #NOTE: uncomment code to perform minute analysis to assure rounding accuracy
     #if timestamp > datetime.datetime.now().timestamp():
-        #print((timestamp - datetime.datetime.now().timestamp())/60)
+        #print((timestamp - datetime.datetime.now().timestamp())/SEC_TO_MINUTE)
 
     #TODO: cache datetime.now() reference for better runtime (at the cost of slightly less accurate time)
-    # or just leave it (at the cost of slower runtime but accurate timing
+    # or just compute at each line (at the cost of slower runtime but accurate timing)
+
+    #FIXME: account for ZeroDivisionError when 0 % 0 occurs!
     if timestamp > datetime.datetime.now().timestamp():
-        if ((timestamp - datetime.datetime.now().timestamp())/60) % \
-                int(((timestamp - datetime.datetime.now().timestamp())/60)) < 0.5:
-            return math.floor(((timestamp - datetime.datetime.now().timestamp())/60))
+        if ((timestamp - datetime.datetime.now().timestamp())/SEC_TO_MINUTE) % \
+                int(((timestamp - datetime.datetime.now().timestamp())/SEC_TO_MINUTE)) < ROUNDING_THRESHOLD:
+            return math.floor(((timestamp - datetime.datetime.now().timestamp())/SEC_TO_MINUTE))
         else:
-            return math.ceil(((timestamp - datetime.datetime.now().timestamp())/60))
+            return math.ceil(((timestamp - datetime.datetime.now().timestamp())/SEC_TO_MINUTE))
     else:
         return -1
