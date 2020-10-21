b138c149c3f14aa15d0481ca562221282f32a9bb
Kesler Mathieu <kmat5@icloud.com>
2020-10-21 12:08:16 -0400

    cleaning up setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+
+#TODO: export permanently
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "pythonpath altered"  $PYTHONPATH
 
 
-# SETUP CMD :
-# sudo apt-get install build-essential libssl-dev libffi-dev python3-dev (for dependency header files needed for cryptography lib)
-# REFERENCE: https://cryptography.io/en/latest/installation/
-# cd MTA-Countdown-Clock-Replica
-# pip3 install -r requirements.txt
+# INSTALL DEPENDENCIES:
+echo "installing header files needed for cryptography lib ..."
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev # REFERENCE: https://cryptography.io/en/latest/installation/
+echo "header files installed!"
 
 
+cd MTA-Countdown-Clock-Replica
+echo "installing python libaries..."
+pip3 install -r requirements.txt
+echo "python libs installed!"
+
+
+# CREATE PRIVATE KEY FILE:
 # source ./setup.sh
 # fetch API_KEY from mta site and generate private key file - {can encapsulate into this setup.sh file and before running app,
-# consider py_api_connect.py file to test validation before adding encrpyted string to config/mta_config.ini and creating keyfile}
+# consider py_api_connect.py file to test validation before adding encrypted string to config/mta_config.ini and creating keyfile}
 
 #RUN CMD:
 # cd mta 
